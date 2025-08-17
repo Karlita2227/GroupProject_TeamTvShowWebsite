37562a21cc7270c3e3aaba9454e967ca60e8d901
Jordan <jordinholenhart@gmail.com>
2025-08-17 11:41:36 -0700

    Tailwind colors set for theme.
    Primary (blue), secondary (orange), accent (light blue) and 2nd accent (yellow) have been added. Moved from index.css to tailwind config for correct use.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,14 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#023047",   // dark blue for navbar
+        secondary: "#FB8500", // orange (buttons)
+        accent: "#219EBC",    // light blue (tagline)
+        highlight: "#FFB703", // yellow (tagline)
+      }
+    },
   },
   plugins: [],
 }
